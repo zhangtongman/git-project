0795778dac4c6bd60c5cdc533fd2fbee2ec74e6b
zhangtongman <132049321+zhangtongman@users.noreply.github.com>
2023-05-06 17:02:19 +0800

    Update App.java

--- src/main/java/com/zpark/ssm/App.java
+++ src/main/java/com/zpark/ssm/App.java
@@ -4,7 +4,8 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println("Hello Git!");
-        System.out.println("Hello GitHub! ztm master first push GitHub!");
+        System.out.println("Hello GitHub! ztm master first push GitHub!");\
+        System.out.println("test pull from GitHub!");
     }
 
 }
