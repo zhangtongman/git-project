9fdca22b82f6684db4e85ab3c97522a3d1d0bdb8
ztm <ztm@163.cn>
2023-05-06 10:16:41 +0800

    ztm master 1 commit!

--- src/main/java/com/zpark/ssm/App.java
+++ src/main/java/com/zpark/ssm/App.java
@@ -4,7 +4,10 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println("Hello Git!");
-        System.out.println("Hello Git!");
+        System.out.println("Hello Git! ztm master second commit!");
+        System.out.println("Hello Git! ztm hotfix first commit!");
+        System.out.println("Hello Git! ztm master first modify!");
+        System.out.println("Hello Git! ztm 1 first modify!");
     }
 
 }
