892276411d72eefce7ea643235e40ee79f5836a0
ztm <ztm@163.cn>
2023-05-06 16:55:01 +0800

    ztm master first1 push Github!

--- src/main/java/com/zpark/ssm/App.java
+++ src/main/java/com/zpark/ssm/App.java
@@ -4,6 +4,7 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println("Hello Git!");
+        System.out.println("Hello GitHub! ztm master first push GitHub!");
     }
 
 }
