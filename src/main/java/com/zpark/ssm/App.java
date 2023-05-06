95440fea955cb8d4c29fa4593274fb305bb16e25
ztm <ztm@163.cn>
2023-05-06 10:17:19 +0800

    ztm master git commit!

--- src/main/java/com/zpark/ssm/App.java
+++ src/main/java/com/zpark/ssm/App.java
@@ -5,6 +5,7 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
         System.out.println("Hello Git!");
+        System.out.println("Hello Git!");
     }
 
 }
